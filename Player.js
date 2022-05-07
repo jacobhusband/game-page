1bb02d104f46d9925003ceb68e18d518a91943dd
Jacob Husband <jakehusband2@gmail.com>
2022-05-07 10:03:33 -0700

    Make a simple Javascript shooting game

---
+++ Player.js
@@ -0,0 +1,114 @@
+export default class Player {
+  constructor(x, y, canvas) {
+    this.x = x;
+    this.y = y;
+    this.canvas = canvas;
+    this.degree = 0;
+    this.width = 25;
+    this.height = 25;
+    this.speed = 4;
+
+    document.addEventListener("keydown", this.keyDownHandler);
+    document.addEventListener("keyup", this.keyUpHandler);
+  }
+
+  draw(ctx) {
+    this.move();
+    ctx.strokeStyle = "yellow";
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    if (this.degree == 0) {
+      ctx.moveTo(this.x, this.y - 12.5);
+      ctx.lineTo(this.x - 25, this.y + 12.5);
+      ctx.lineTo(this.x + 25, this.y + 12.5);
+      ctx.lineTo(this.x, this.y - 12.5);
+    } else if (this.degree == 180) {
+      ctx.moveTo(this.x, this.y + 12.5);
+      ctx.lineTo(this.x - 25, this.y - 12.5);
+      ctx.lineTo(this.x + 25, this.y - 12.5);
+      ctx.lineTo(this.x, this.y + 12.5);
+    } else if (this.degree == 90) {
+      ctx.moveTo(this.x + 12.5, this.y);
+      ctx.lineTo(this.x - 12.5, this.y + 25);
+      ctx.lineTo(this.x - 12.5, this.y - 25);
+      ctx.lineTo(this.x + 12.5, this.y);
+    } else if (this.degree == 270) {
+      ctx.moveTo(this.x - 12.5, this.y);
+      ctx.lineTo(this.x + 12.5, this.y + 25);
+      ctx.lineTo(this.x + 12.5, this.y - 25);
+      ctx.lineTo(this.x - 12.5, this.y);
+    }
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  move() {
+    if (this.rightPressed == true && this.upPressed == true) {
+      this.x += this.speed;
+      this.y -= this.speed;
+      this.degree = 45;
+    } else if (this.rightPressed == true && this.downPressed == true) {
+      this.x += this.speed;
+      this.y += this.speed;
+      this.degree = 135;
+    } else if (this.leftPressed == true && this.upPressed == true) {
+      this.x -= this.speed;
+      this.y -= this.speed;
+      this.degree = 315;
+    } else if (this.leftPressed == true && this.downPressed == true) {
+      this.x -= this.speed;
+      this.y += this.speed;
+      this.degree = 215;
+    } else if (this.leftPressed == true && this.rightPressed == true) {
+      this.x += this.speed;
+      this.degree = 0;
+    } else if (this.upPressed == true && this.downPressed == true) {
+      this.y -= this.speed;
+      this.degree = 0;
+    } else if (this.rightPressed == true) {
+      this.x += this.speed;
+      this.degree = 90;
+    } else if (this.leftPressed == true) {
+      this.x -= this.speed;
+      this.degree = 270;
+    } else if (this.upPressed == true) {
+      this.y -= this.speed;
+      this.degree = 0;
+    } else if (this.downPressed == true) {
+      this.y += this.speed;
+      this.degree = 180;
+    }
+  }
+
+  keyDownHandler = (event) => {
+    if (event.keyCode == 68) {
+      this.rightPressed = true;
+    } else if (event.keyCode == 65) {
+      this.leftPressed = true;
+    }
+    if (event.keyCode == 83) {
+      this.downPressed = true;
+    } else if (event.keyCode == 87) {
+      this.upPressed = true;
+    }
+    if (event.keyCode == 32) {
+      this.spacePressed = true;
+    }
+  };
+
+  keyUpHandler = (event) => {
+    if (event.keyCode == 68) {
+      this.rightPressed = false;
+    } else if (event.keyCode == 65) {
+      this.leftPressed = false;
+    }
+    if (event.keyCode == 83) {
+      this.downPressed = false;
+    } else if (event.keyCode == 87) {
+      this.upPressed = false;
+    }
+    if (event.keyCode == 32) {
+      this.spacePressed = false;
+    }
+  };
+}
---
+++ index.js
@@ -0,0 +1,68 @@
+import Player from "./Player.js";
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+let spacePressed = false;
+let tickX = 145;
+let tickY = 63;
+let degree = 0;
+const triangleWH = 10;
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 550;
+canvas.height = 600;
+
+const player = new Player(canvas.width / 2, canvas.height / 2, canvas);
+
+function gameLoop() {
+  setCommonStyle();
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw(ctx);
+}
+
+function setCommonStyle() {
+  ctx.shadowColor = "#c14953";
+  ctx.shadowBlur = 20;
+  ctx.lineJoin = "bevel";
+  ctx.lineWidth = 5;
+}
+
+setInterval(gameLoop, 1000 / 60);
+
+// let bullet = {
+//   x: tickX,
+//   y: tickY,
+//   dx: 0,
+//   dy: 0,
+// };
+
+// let image = new Image();
+// image.src = "./Sprites/Triangle.png";
+
+// playButton.addEventListener("click", (event) => playGame(event));
+
+// const playGame = function (event) {
+//   // x: 145, y: 63 is the center of the screen
+//   ctx.drawImage(image, 145, 63, 10, 10);
+// };
+
+// function drawRotation(degrees) {
+//   let x = canvas.width / 2;
+//   let y = canvas.height / 2;
+//   let w = image.width / 30;
+//   let h = image.height / 30;
+//   let rads = (degrees * Math.PI) / 180;
+
+//   ctx.translate(tickX, tickY);
+//   ctx.rotate(rads);
+//   ctx.drawImage(image, -w / 2, -h / 2, w, h);
+//   ctx.rotate(-rads);
+//   ctx.translate(-tickX, -tickY);
+// }
+
+// window.requestAnimationFrame(draw);
